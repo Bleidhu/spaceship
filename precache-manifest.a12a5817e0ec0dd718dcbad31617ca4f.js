c2beabc27f5705fe7afaba62fb7113f57f0126c9
Bleidhu <ichigosr2014@gmail.com>
2024-04-17 22:22:33 +0200

    Updates

---
+++ precache-manifest.a12a5817e0ec0dd718dcbad31617ca4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da63165ecf41ad19801c66d1ac72c63a",
+    "url": "/spaceship/index.html"
+  },
+  {
+    "revision": "69ca990a6a23a62e3b28",
+    "url": "/spaceship/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "78e26e6354796a098510",
+    "url": "/spaceship/static/css/main.2848b019.chunk.css"
+  },
+  {
+    "revision": "69ca990a6a23a62e3b28",
+    "url": "/spaceship/static/js/2.29b265f0.chunk.js"
+  },
+  {
+    "revision": "cb9c0b9fd444fb2a488f",
+    "url": "/spaceship/static/js/3.d9c1e0bb.chunk.js"
+  },
+  {
+    "revision": "78e26e6354796a098510",
+    "url": "/spaceship/static/js/main.6c512553.chunk.js"
+  },
+  {
+    "revision": "295012257bc435985339",
+    "url": "/spaceship/static/js/runtime~main.44d26804.js"
+  }
+]);
